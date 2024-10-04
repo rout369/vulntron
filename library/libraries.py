d22770a0f6210065b356ef404ea72b01145f2f6a
Biswajit Rout <146954686+rout369@users.noreply.github.com>
2024-10-04 14:38:05 +0530

    Create libraries.py

---
+++ library/libraries.py
@@ -0,0 +1,19 @@
+import socket 
+import time
+from scapy.all import *
+from scapy.all import Ether, ARP, srp
+import sys
+from colorama import init, Fore, Style
+from tqdm import tqdm
+import subprocess
+import re
+import requests
+import phonenumbers
+from phonenumbers import geocoder, carrier
+from opencage.geocoder import OpenCageGeocode
+import folium
+from colorama import Fore, Style
+from bs4 import BeautifulSoup
+import subprocess
+import os
+import platform
