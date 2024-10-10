aa3e06b1649ceda69a9bed5e9d1e31e795c391aa
Biswajit Rout <146954686+rout369@users.noreply.github.com>
2024-10-10 22:36:59 +0530

    Update Log.py

--- lib/helper/Log.py
+++ lib/helper/Log.py
@@ -1,22 +1,50 @@
 from lib.helper.helper import * 
-from datetime import datetime
-
-# Color codes for console output
-Y = "\033[93m"  # Yellow
-G = "\033[92m"  # Green
-R = "\033[91m"  # Red
-N = "\033[0m"   # Reset
+import sys
 
 class Log:
+    
+    # ANSI color codes
+    Y = "\033[1;33m"  # Yellow
+    G = "\033[1;32m"  # Green
+    R = "\033[1;31m"  # Red
+    M = "\033[1;35m"  # Magenta
+    N = "\033[0m"     # Reset
+
+    # Emojis for different log levels
+    EMOJIS = {
+        "INFO": "ℹ️",
+        "WARNING": "⚠️",
+        "CRITICAL": "🚨"
+    }
+
+    # Terminal prompt representation with magenta color
+    PROMPT = f"{M}┌──(XssXplore 🕵️‍♂️SCANNER)-[㉿]\n└─$-o> {N}"
+
+    @classmethod
+    def _format_message(cls, level, text):
+        emoji = cls.EMOJIS.get(level, "")
+        color = cls.G if level == "INFO" else (cls.Y if level == "WARNING" else cls.R)
+        return f"{cls.PROMPT} {emoji} {color}{level}:{cls.N} {text}"
 
     @classmethod
     def info(cls, text):
-        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] [" +"ℹ️"+ G + "INFO" + N + "] " + text)
+        print(cls._format_message("INFO", text))
 
     @classmethod
     def warning(cls, text):
-        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] ["  +"⚠️" + Y + "WARNING" + N + "] " + text)
+        print(cls._format_message("WARNING", text))
 
     @classmethod
     def high(cls, text):
-        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] ["  +"🚨" + R + "CRITICAL" + N + "] " + text)
+        print(cls._format_message("CRITICAL", text))
+
+    @classmethod
+    def set_terminal_style(cls):
+        print(f"{cls.M}┌──(XssXplore📡Log){cls.N}")
+        print(f"{cls.M}└──#-o>{cls.N}",Log.info("Connection established successfully!"))
+
+# # Usage
+Log.set_terminal_style()
+# Log.info("Connection established successfully!")
+# Log.warning("Potential XSS vulnerability detected at https://example.com/vulnerable")
+# Log.high("Detected XSS (GET) at https://example.com/attack")
