686f4bf3e8bbf6f92eee4a423d0e9ad88c018eff
Biswajit Rout <146954686+rout369@users.noreply.github.com>
2024-10-10 22:44:41 +0530

    Update helper.py

--- lib/helper/helper.py
+++ lib/helper/helper.py
@@ -14,25 +14,44 @@ underline = "\033[4m"
 agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} 
 line="—————————————————" 
 #####################
-def session(proxies,headers,cookie):
-	r=requests.Session()
-	r.proxies=proxies
-	r.headers=headers
-	r.cookies.update(json.loads(cookie))
-	return r
+def session(proxies, headers, cookie):
+    """
+    Create a requests session with specified proxies, headers, and cookies.
 
-logo=G+"""
+    Parameters:
+    - proxies (dict): The proxies to use for the session.
+    - headers (dict): The headers to include in the session.
+    - cookie (str): A JSON string of cookies to set in the session.
 
-                      __   __        ___                          
+    Returns:
+    - requests.Session: Configured session object.
+    """
+    r = requests.Session()
+    r.proxies = proxies
+    r.headers = headers
+    try:
+        r.cookies.update(json.loads(cookie))
+    except json.JSONDecodeError:
+        print(f"{R}Error: Invalid cookie format.{N}")
+    return r
+
+
+logo=G+"""                      __   __        ___                          
                      /\ \ /\ \      /\_ \                         
  __  _   ____    ____\ `\`\/'/' ____\//\ \     ___   _ __    __   
 /\ \/'\ /',__\  /',__\`\/ > <  /\ '__`\\ \ \   / __`\/\`'__\/'__`\ 
-\/>  <//\__, `\/\__, `\  \/'/\`\\ \ \L\ \\_\ \_/\ \L\ \ \ \//\  __/ 
+\/>  <//\__, `\/\__, `\  \/'/\`\\ \ \L\ \\_\ \_/\ \L\ \ \ \//\  __/ %s
  /\_/\_\/\____/\/\____/  /\_\\ \_\ \ ,__//\____\ \____/ \ \_\\ \____\
- 
+%s
  \//\/_/\/___/  \/___/   \/_/ \/_/\ \ \/ \/____/\/___/  \/_/ \/__/
                                    \ \_\                            
                                     \/_/
 
-<<<<<<< HEAD                         
-""" 
+<<<<<<< STARTING >>>>>>>
+"""%(R+"{v1.0 Devloping}"+G,underline+C+"https://github.com/rout369/vulntron"+N+G)
+	
+##=======
+"""%(R+"{v1.0 Devloping}"+G,underline+C+"https://github.com/rout369/vulntron"+N+G)
+	
+>>>>>>> branch 'master' of https://github.com/rout369/vulntron
+"""
