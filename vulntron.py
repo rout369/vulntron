1b06686b56a64773297711e5a9ea086b238ed200
Biswajit Rout <146954686+rout369@users.noreply.github.com>
2024-10-10 22:17:06 +0530

    Update vulntron.py

--- vulntron.py
+++ vulntron.py
@@ -1,201 +1,85 @@
+import argparse
 from library.libraries import *
 from tool.intro import *
 from tool.port import *
-from tool.phone import *
 from tool.wifi import *
 from vuln.command import open_new_command_line
 
 
-init(autoreset=True)
+def display_help():
+    display_colored_intro()
+    print("")
+    help_text = """
+    Usage: python Vulntron.py [OPTIONS]
 
-# Function to display a loading bar with colors
-def show_loading_bar(duration):
-    print(Fore.YELLOW + Style.BRIGHT + "Initializing the VULNTRON...")
-    # Using tqdm to create a loading bar
-    for _ in tqdm(range(duration), desc="Loading", bar_format="{l_bar}{bar} | {n_fmt}/{total_fmt} seconds"):
-        time.sleep(1)  # Simulating loading time
+    Vulntron: A comprehensive ethical hacking tool for vulnerability analysis.
 
-    print(Fore.CYAN + "\nInitialization complete!\n")
-
-
-
-
-class bcolors:
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    RESET = '\033[0m'
-
-conf.L3socket = conf.L3socket()
-
-
-
-def network_scanner():
-
-    print(Fore.YELLOW + Style.BRIGHT + "Initializing the network scanner...", end="")
-    for _ in range(5):
-        # Simulate loading with colored dots
-        print(Fore.GREEN + ".", end="")
-        sys.stdout.flush()
-        time.sleep(1)
-    print(Fore.CYAN + "\nInitialization complete!\n")
-
-
-
-
-    header_art = r"""
-        [-]-----------------------------------------[-]
-        _______          __                       __       
-        \      \   _____/  |___  _  _____________|  | __   
-        /   |   \_/ __ \   __\ \/ \/ /  _ \_  __ \  |/ /   
-       /    |    \  ___/|  |  \     (  <_> )  | \/    <    
-       \____|__  /\___  >__|   \/\_/ \____/|__|  |__|_ \   
-               \/     \/                              \/   
-        _________                                         
-        /   _____/ ____ _____    ____   ____   ___________ 
-        \_____  \_/ ___\\__  \  /    \ /    \_/ __ \_  __ \
-        /        \  \___ / __ \|   |  \   |  \  ___/|  | \/
-        /_______  /\___  >____  /___|  /___|  /\___  >__|   
-                \/     \/     \/     \/     \/     \/       
-        [-]-----------------------------------------[-] """
-    
-    print(bcolors.RED + header_art)
-
-    print(" ")
-
-   
-
-    while True:
-        print(" ")
-        print(bcolors.RED + '['+bcolors.YELLOW+'::' + bcolors.RED + ']' + bcolors.GREEN + '  Select What You Want To Do  ' + bcolors.RED + '['+bcolors.YELLOW+'::' + bcolors.RED + ']')
-        print(" ")
-        print(bcolors.RED + '['+bcolors.YELLOW+'01' + bcolors.RED + ']' + bcolors.GREEN + '  Port_scanner ' + bcolors.RESET )
-        print(bcolors.RED + '['+bcolors.YELLOW+'02' + bcolors.RED + ']' + bcolors.GREEN + '  PhoneSphere ' + bcolors.RESET )
-        print(bcolors.RED + '['+bcolors.YELLOW+'03' + bcolors.RED + ']' + bcolors.GREEN + '  WiFiWhisperer ' + bcolors.RESET )
-        choice = input(Fore.YELLOW + "Enter your choice or 'q' to quit: ")
-        if choice == '1':
-            port_scanner()
-        elif choice == '2':
-            phone_number = input("Enter a phone number: ")
-            get_phone_info(phone_number)
-        elif choice == '3':
-            print(Fore.YELLOW + Style.BRIGHT + "Gathering _Wifi_Information...", end="")
-            for _ in range(5):
-                print(Fore.GREEN + ".", end="")
-                sys.stdout.flush()
-                time.sleep(1)
-
-            wifi_info = get_connected_wifi_details()
-            if wifi_info:
-                print(Fore.GREEN + "\nWi-Fi Information:")
-                for key, value in wifi_info.items():
-                    print(Fore.YELLOW + f"{key}: {Fore.CYAN + value}")
-
-        elif choice.lower() == 'q':
-            print("Stopping the tool....")
-            break;  # This will stop the listener thread1
-        else:
-            print(Fore.RED + "Invalid choice, please try again.")
-
-
-  
-
-def Vulnerablity_scanner():
-    print(Fore.YELLOW + Style.BRIGHT + "Initializing the Vulnerability Scanner...", end="")
-    for _ in range(5):
-        # Simulate loading with colored dots
-        print(Fore.GREEN + ".", end="")
-        sys.stdout.flush()
-        time.sleep(1)
-    print(Fore.CYAN + "\nInitialization complete!\n")
-
-    header_art = r"""
-          ,;~;,                                                     |   |   |
-                /\_                                                 |   |___|
-               (  /                                                 |___|   |
-               (()      //)      [::]vulnerability Scanner[::]      |   |   |
-               | \\  ,,;;'\                                         |   |___|
-           __ _(  )m=(((((((((((((================--------          |___|   |
-         /'  ' '()/~' '.(, |                                        |   |   |
-      ,;(      )||     |  ~                                         |   |___| 
-     ,;' \    /-(.;,   )                                            |___|   |
-          ) /       ) /                                             |   |   |
-         //         ||  version 1.0                                 |   |___|
-        )_\         )_\                                             |   |   |
+    Options:
+        --show-intro,       Display the tool description.
+        --port-scanner, -Ps Launch the port scanner.
+        --wifi, -W          Launch the Wi-Fi Information Gathering tool.
+        --Xss               Launch New command line for XssScanner.
+        --Sql               Launch New command line for SQLScanner.
+        --help-info         Display this help information.
     """
+    print(Fore.YELLOW + help_text)
+
+def main():
+    # Argument parser for command-line options
+    parser = argparse.ArgumentParser(description='Vulntron: A comprehensive ethical hacking tool for vulnerability analysis')
     
-    print(bcolors.GREEN + header_art)
-    print(" ")
+    # Add an argument for showing the colored intro
+    parser.add_argument('--show-intro',action='store_true',help="Display the tool description.")
+    # parser.add_argument('--show-description', action='store_true', help='Display the tool description')
+    parser.add_argument('--port-scanner','-Ps', action='store_true', help="Launch the port scanner.")
+    parser.add_argument('--wifi','-W', action='store_true', help="Launch the WI-Fi Information Gathering tool.")
+    parser.add_argument('--Xss', action='store_true', help="Launch New command line for XssScanner.")
+    parser.add_argument('--Sql', action='store_true', help="Launch New command line for SQLScanner.")
+    parser.add_argument('--help-info', action='store_true', help="Display help information about command-line options.")
+    args = parser.parse_args()
 
-    while True:
-        print(" ")
-        print(bcolors.RED + '['+bcolors.YELLOW+'::' + bcolors.RED + ']' + bcolors.GREEN + '  Select What You Want To Do  ' + bcolors.RED + '['+bcolors.YELLOW+'::' + bcolors.RED + ']')
-        print(" ")
-        print(bcolors.RED + '['+bcolors.YELLOW+'01' + bcolors.RED + ']' + bcolors.GREEN + '  XSS_Scanner ' + bcolors.RESET)
-        print(bcolors.RED + '['+bcolors.YELLOW+'02' + bcolors.RED + ']' + bcolors.GREEN + '  SQL_Injection' + bcolors.RESET)
-        choice = input(Fore.YELLOW + "Enter your choice or 'q' to quit: ")
-        print(" ")
-        if choice == '1':
-            print("Use the command python xssxplore.py to start the scanner")
-            print(Fore.YELLOW + Style.BRIGHT + "Initializing the Xss Scanner...", end="")
-            for _ in range(5):
+    # If the user passes the --show-intro flag, show the intro
+    if args.help_info:
+        display_help()
+    if args.show_intro:
+        display_colored_intro()
+    if args.port_scanner:
+        network_scanner()
+        port_scanner()
+    if args.wifi:
+        wifi()
+        print(Fore.YELLOW + Style.BRIGHT + "Gathering _Wifi_Information...", end="")
+        for _ in range(5):
+            print(Fore.GREEN + ".", end="")
+            sys.stdout.flush()
+            time.sleep(1)
+        wifi_info = get_connected_wifi_details()
+        if wifi_info:
+            print(Fore.GREEN + "\nWi-Fi Information:")
+            for key, value in wifi_info.items():
+                print(Fore.YELLOW + f"{key}: {Fore.CYAN + value}")       
+    if args.Xss:
+        Vulnerablity_scanner()
+        print("Use the command python xssxplore.py to start the scanner")
+        print(Fore.YELLOW + Style.BRIGHT + "Initializing the Xss Scanner...", end="")
+        for _ in range(5):
         # Simulate loading with colored dots
                 print(Fore.GREEN + ".", end="")
                 sys.stdout.flush()
                 time.sleep(1)
-            print(Fore.CYAN + "\nInitialization complete!\n")
-            open_new_command_line()  # Opens a new command line window
-        elif choice == '2':
-            print("Use the command python inject.py --help to start the scanner")
-            print(Fore.YELLOW + Style.BRIGHT + "Initializing the Xss Scanner...", end="")
-            for _ in range(5):
+        print(Fore.CYAN + "\nInitialization complete!\n")
+        open_new_command_line()
+    if args.Sql:
+        Vulnerablity_scanner()
+        print("Use the command python inject.py --help to start the scanner")
+        print(Fore.YELLOW + Style.BRIGHT + "Initializing Sql Injection Scanner...", end="")
+        for _ in range(5):
         # Simulate loading with colored dots
                 print(Fore.GREEN + ".", end="")
                 sys.stdout.flush()
                 time.sleep(1)
-            print(Fore.CYAN + "\nInitialization complete!\n")
-            open_new_command_line()  # Opens a new command line window
-        elif choice.lower() == 'q':
-            print("Stopping the tool....")
-            break
-
-        else:
-            print(Fore.RED + "Invalid choice, please try again.")
- 
-
-
-
-
-
-
-
-def main_menu():
-    while True:
-        print(" ")
-        print(bcolors.RED + '['+bcolors.YELLOW+'::' + bcolors.RED + ']' + bcolors.GREEN + '  Select What You Want To Do  ' + bcolors.RED + '['+bcolors.YELLOW+'::' + bcolors.RED + ']')
-        print(" ")
-        # print(" ")
-        # print(" ")
-        print(bcolors.RED + '['+bcolors.YELLOW+'01' + bcolors.RED + ']' + bcolors.GREEN + '  Network Scanner ' + bcolors.RESET )
-        print(bcolors.RED + '['+bcolors.YELLOW+'02' + bcolors.RED + ']' + bcolors.GREEN + '  Vulnerability Scanners' + bcolors.RESET )
-        choice = input(Fore.YELLOW + "Enter your choice or 'q' to quit: ")
-        print(" ")
-        if choice == '1':
-            network_scanner()
-        elif choice == '2':
-            Vulnerablity_scanner()
-        elif choice.lower() == 'q':
-            print("Exiting...")
-            break
-        else:
-            print(Fore.RED + "Invalid choice, please try again.")
-
-
-
-show_loading_bar(3)  # 3 seconds of loading time
-
-# Call the function to display the colored ASCII art
-display_colored_intro()
-
-main_menu() # menu driven
-
+        print(Fore.CYAN + "\nInitialization complete!\n")
+        open_new_command_line()
+           
+if __name__ == '__main__':
+    main()
